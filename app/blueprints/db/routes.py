d8a249fad6e882f7f8cbe0658fc32537b5244cbc
Denys Kamiskyi <135739796+dionisiyKDO@users.noreply.github.com>
2024-09-21 20:50:36 +0300

    feat: top_count_of_plays endpoint

--- app/blueprints/db/routes.py
+++ app/blueprints/db/routes.py
@@ -31,7 +31,7 @@ def get_record(id):
 
 # Route for getting top played tracks by time played
 @db_bp.route('/top_played_tracks')
-def get_total_played(limit=10):
+def get_top_played_tracks(limit=10):
     if 'token_info' not in session:
         return jsonify({'error': 'Unauthorized'}), 401
     
@@ -65,6 +65,42 @@ def get_total_played(limit=10):
             'album_image_url': sp.track(track_id=spotify_track_uri.replace("spotify:track:", ""))['album']['images'][0]['url']
         }
         for index, (track_name, track_artist_name, spotify_track_uri, total_ms_played) in enumerated_query
+    ]
+    
+    return jsonify(track_playtime_dict)
+
+# Route for getting top played tracks by time played
+@db_bp.route('/top_count_of_plays')
+def get_top_count_of_plays(limit=10):
+    if 'token_info' not in session:
+        return jsonify({'error': 'Unauthorized'}), 401
+    
+    sp = get_spotify_client()
+    
+    query = ( # query for track name, artist and total ms played, grouped by track name to sum up total ms played, and order by total ms played descending
+        db_session.query(
+            StreamingHistory.master_metadata_track_name,
+            StreamingHistory.master_metadata_album_artist_name,
+            StreamingHistory.spotify_track_uri,
+            func.count(StreamingHistory.ts).label('count_of_plays')
+        )
+        .filter(StreamingHistory.master_metadata_track_name.isnot(None))  # Exclude None track names
+        .group_by(StreamingHistory.master_metadata_track_name, StreamingHistory.master_metadata_album_artist_name)
+        .order_by(desc('count_of_plays'))
+        .limit(limit) # how many top tracks to return
+    )
+    
+    enumerated_query = enumerate(query)
+
+    # Convert the result into a dictionary
+    track_playtime_dict = [ {
+            'index': index,
+            'name': track_name,
+            'artist': track_artist_name,
+            'count_of_plays': count_of_plays,
+            'album_image_url': sp.track(track_id=spotify_track_uri.replace("spotify:track:", ""))['album']['images'][0]['url']
+        }
+        for index, (track_name, track_artist_name, spotify_track_uri, count_of_plays) in enumerated_query
     ]
     
     return jsonify(track_playtime_dict)

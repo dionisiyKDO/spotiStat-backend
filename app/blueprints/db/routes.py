700e5de10374badadd882e07d402e97f6e7bdfe2
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-06-13 12:41:54 +0300

    little refactor

--- app/blueprints/db/routes.py
+++ app/blueprints/db/routes.py
@@ -1,43 +1,38 @@
 from flask import jsonify, session, current_app, request
 from sqlalchemy import func, desc, extract
+from datetime import datetime
 import pandas as pd
+import os
 
 # from app.blueprints.auth.routes import get_spotify_client
-from app.utils.utils import *
+from app.utils.upload_utils import read_json_and_store_data
 from app.models import StreamingHistory, User
+from app.database import db_session
 from . import db_bp
 
-# TODO: think about how to handle same songs, but in different albums, etc. Stats should summed up, but they are split as they are technically different songs
+MS_IN_HOUR = 1000 * 60 * 60
 
-@db_bp.route('/check_history')
-def check_history():
-    account_id = request.args.get('account_id')
-
-    if not os.path.exists(f"app/data/{account_id}"):
-        return jsonify({'error': 'No history found for this account'}), 404
-    else:
-        return jsonify({'message': 'History found for this account'})
 
 # NOW it is route for importing json files from pc, and storing them in sqllite db
 # Ideally, it should be a route to accept json files from the frontend, and store them in the db
 # @db_bp.route('/upload_history', methods=['POST'])
+# def upload_history():
+#     if not request.is_json:
+#         return jsonify({'error': 'Invalid input, expected JSON'}), 400
+#     data = request.get_json()
+#     try:
+#         tmp = read_json_and_store_data(json_data=data)
+#     except Exception as e:
+#         return jsonify({'error': f'Failed to process data: {str(e)}'}), 500
 @db_bp.route('/upload_history')
 def upload_history():
     username = str(request.args.get('username', None))
-    if username == None:
+    if username is None:
         return jsonify({'error': 'No username provided'}), 404
     
-    # if not request.is_json:
-    #     return jsonify({'error': 'Invalid input, expected JSON'}), 400
-    # data = request.get_json()
-    # try:
-    #     tmp = read_json_and_store_data(json_data=data)
-    # except Exception as e:
-    #     return jsonify({'error': f'Failed to process data: {str(e)}'}), 500
-    
-    tmp = read_json_and_store_data(json_directory=os.path.join('./app/data/', username))
+    result = read_json_and_store_data(json_directory=os.path.join('./app/data/', username))
 
-    return jsonify({'success': tmp})
+    return jsonify({'success': result})
 
 
 
@@ -47,42 +42,33 @@ def upload_history():
 
 
 
-MS_IN_HOUR = 1000 * 60 * 60
 
 @db_bp.route('/history/track/<track_id>/stats', methods=['GET'])
 def get_track_stats(track_id):
     username = str(request.args.get('username', None))
-    print('requested history/track/<track_id>/stats : with username = ', username)
     if username is None:
         return jsonify({'error': 'No username provided'}), 404
     
-    # Strip the "spotify:track:" prefix from the URI for both queries
     track_uri = f"spotify:track:{track_id}"
     
-    # Query to get play count and total playtime (ms_played) per day for the track
+    # Query to get play_count and total_playtime (ms_played) per day for the track
     play_counts = (
         db_session.query(
             func.date(StreamingHistory.ts).label('date'), 
             func.count(StreamingHistory.ts).label('play_count'),
             func.sum(StreamingHistory.ms_played).label('total_ms_played')  # Adding total playtime per day
         )
-        .filter((StreamingHistory.spotify_track_uri == track_uri) &
-                (StreamingHistory.username == username))
+        .filter(
+            (StreamingHistory.username == username) &
+            (StreamingHistory.spotify_track_uri == track_uri))                      # by track uri
+            # (StreamingHistory.master_metadata_track_name.ilike(f'%{track_id}%'))  # by track name (different versions of the same track)
         .group_by(func.date(StreamingHistory.ts))
         .order_by(func.date(StreamingHistory.ts))
         .all()
-        # db_session.query(
-        #     func.date(StreamingHistory.ts).label('date'), 
-        #     func.count(StreamingHistory.ts).label('play_count'),
-        #     func.sum(StreamingHistory.ms_played).label('total_ms_played')  # Adding total playtime per day
-        # )
-        # .filter(StreamingHistory.master_metadata_track_name.ilike(f'%{track_id}%'))
-        # .group_by(func.date(StreamingHistory.ts))
-        # .order_by(func.date(StreamingHistory.ts))
-        # .all()
     )
 
-    # Query to get total playtime (ms_played) and other interesting stats
+    # Query to get total_playtime (ms_played) and other interesting stats
+    # TODO: maybe get that info from query that already made - play_counts
     track_stats = (
         db_session.query(
             func.sum(StreamingHistory.ms_played).label('total_ms_played'),
@@ -91,8 +77,9 @@ def get_track_stats(track_id):
             func.max(StreamingHistory.ts).label('last_played'),
             func.count(func.distinct(func.date(StreamingHistory.ts))).label('distinct_days_played')
         )
-        .filter((StreamingHistory.spotify_track_uri == track_uri) &
-                (StreamingHistory.username == username))
+        .filter((StreamingHistory.username == username) &
+                (StreamingHistory.spotify_track_uri == track_uri))                      # by track uri
+                # (StreamingHistory.master_metadata_track_name.ilike(f'%{track_id}%'))  # by track name (different versions of the same track)
         .one()
     )
     

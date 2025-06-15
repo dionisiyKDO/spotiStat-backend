c1ba3b37ec7ddbe496dfa0ce6bc2598aa0bb3d6b
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-06-15 17:21:20 +0300

    replace routes from utils file to routes file

--- app/blueprints/db/__init__.py
+++ app/blueprints/db/__init__.py
@@ -1,7 +1,5 @@
 from flask import Blueprint
 
 db_bp = Blueprint('database', __name__, url_prefix='/db')
-# TODO: rethink database logic. Not store everything in one table, split it up by years?, or half years?
-# Queries on specific dates take too long
 
-from . import routes, utils
+from . import routes

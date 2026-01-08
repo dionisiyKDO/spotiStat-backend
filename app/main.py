2913b62bbbfcf4a43c7c18d8d04f609cd4327577
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2026-01-08 10:51:28 +0000

    migrate some endpoints

--- app/main.py
+++ app/main.py
@@ -21,7 +21,33 @@ app = FastAPI()
 async def read_user_me():
     return {"Hello": "World!"}
 
-@app.get("/calculate_stats")
-async def calculate_stats(db: Session = Depends(get_db)):
-    result = StatsManager.calculate_stats_for_user('dionisiy', db)
-    return result
+@app.get("/stats/{username}")
+async def calculate_stats(username: str, db: Session = Depends(get_db)):
+    try:
+        if StatsManager.stats_exist_for_user(username, db):
+            stats = StatsManager.get_stats_for_user(username, db)
+            return stats
+        else:
+            stats = StatsManager.calculate_stats_for_user(username, db)
+            return {
+                'message': f'Stats calculated successfully for {username}',
+                'data': stats
+            }
+    except Exception as e:
+        return {'error': f'Error calculating stats: {str(e)}'}, 500
+
+@app.get("/stats/{username}/status")
+async def stats_status(username: str, db: Session = Depends(get_db)):
+    try:
+        return StatsManager.stats_exist_for_user(username, db)
+    except Exception as e:
+        return {'error': f'Error calculating stats: {str(e)}'}, 500
+
+@app.get('/stats/{username}/{stat}')
+def get_total_listening_time(username: str, stat: str, db: Session = Depends(get_db)):
+    ''' Display the total listening time in ms/min/hour/day '''
+    stats = StatsManager.get_stats_for_user(username, db)
+    if not stats:
+        return {'error': 'Stats not found. Please calculate stats first.'}, 404
+    print(stats)
+    return stats[stat]
